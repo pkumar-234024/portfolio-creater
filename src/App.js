4fc88d4908b57fde9e2d19a9a28c87829b748ec0
praveen kumar <praveen991210@gmail.com>
2023-09-10 19:48:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
