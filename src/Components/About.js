3b0beb5209bb0fdb13375a7be8b87203a77fda50
praveen kumar <praveen991210@gmail.com>
2023-09-14 12:56:10 +0530

    about

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <>
+      <h1>TThis is all about me</h1>
+      </>
+    </div>
+  )
+}
+
+export default About
+<>
+<h1>TThis is all about me</h1>
+</>
